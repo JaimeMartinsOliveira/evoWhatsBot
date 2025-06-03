0278490a87439985147197472674e9ec9330407f
jaimemartinsoliveira <1072224389@ulife.com.br>
2025-06-02 23:59:25 -0300

    create webhook

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-import fastapi
+from fastapi import FastAPI, Request
 
-api = fastapi.FastAPI()
+app = FastAPI()
+
+
+@app.post("/webhook")
+async def webhook_listener(request: Request):
+    payload = await request.json()
+    print("Recebido webhook:", payload)
+
+    return {"message": "Webhook recebido com sucesso"}
